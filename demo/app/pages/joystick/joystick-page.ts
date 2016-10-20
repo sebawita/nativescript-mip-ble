491d5159e6c191afd254038472ed619b44e60228
Sebastian Witalec <sebawita@gmail.com>
2016-10-20 12:36:57 +0200

    Changing to use a singleton, so that the state remains between pages

--- demo/app/pages/joystick/joystick-page.ts
+++ demo/app/pages/joystick/joystick-page.ts
@@ -1,10 +1,10 @@
 import { EventData } from "data/observable";
 import { Page } from "ui/page";
-import { JoyStickViewModel } from "./joystick-view-model";
+import { JoyStick } from "./joystick-view-model";
 
 // Event handler for Page "navigatingTo" event attached in main-page.xml
 export function navigatingTo(args: EventData) {
     // Get the event sender
     var page = <Page>args.object;
-    page.bindingContext = new JoyStickViewModel();
+    page.bindingContext = JoyStick;
 }
