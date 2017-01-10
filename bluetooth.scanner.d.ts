43622f721a48ceb28d708e1f37af3e930967e463
Sebastian Witalec <sebawita@gmail.com>
2017-01-10 11:54:31 +0000

    Added filter to device scanner to only return MiP devices

--- bluetooth.scanner.d.ts
+++ bluetooth.scanner.d.ts
@@ -1,6 +1,7 @@
 export declare class BluetoothScanner {
     constructor();
     scan(deviceFound: (MipDevice) => any): Promise<any>;
+    checkIfMiP(periperhal: any): boolean;
     initialisePermissionsIfRequired(): void;
     hasPermissions(): Promise<boolean>;
     requestPermissions(): void;
--- bluetooth.scanner.ts
+++ bluetooth.scanner.ts
@@ -10,22 +10,21 @@ export class BluetoothScanner {
     }
 
     scan(deviceFound: (MipDevice) => any): Promise<any> {
+
         return bluetooth.startScanning({
             serviceUUIDs: [],
             seconds: 3,
             onDiscovered: peripheral => {
-                if (peripheral.name === null) {
-                    console.log("Skipping a periperhal without a name with UUID:" + peripheral.UUID);
+                if (this.checkIfWowWeeMiP(peripheral) === false) {
+                    console.log(`Skipping a non mip device with UUID: ${peripheral.UUID} and advertisement: ${peripheral.advertisement}`);
                     return;
                 }
-                // Should I check the advertisement to verify if this is a MipDevice
-                //peripheral.advertisement ???===??? "AgEGBQLw/7D/E/8ABQUAAAAAAAAAAAAAAAAAAAANCVdvd1dlZS1NaXB1cwUSCAAQAAIKAAIKAAAAAAAAAAA="
-                // Non Mip example: AgEGEQa6Vomm+r+ivQFGfW7XYqutBRYKGAcEBQlGbGV4Agr6AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=
 
                 console.log("");
                 console.log("----New Device Found----");
                 console.log("Periperhal found with UUID: " + peripheral.UUID);
                 console.log("Periperhal found with name: " + peripheral.name);
+                console.log("Periperhal found with advertisement: " + peripheral.advertisement);
 
                 var newDevice = new MipDevice(peripheral.UUID, peripheral.name, peripheral.state);
 
@@ -34,6 +33,21 @@ export class BluetoothScanner {
         });
     }
 
+    checkIfWowWeeMiP(periperhal: any) {
+        const advert = base64_decode(periperhal.advertisement);
+
+        /* check if a WowWee device
+           Wowwee has a code 0x03FO, they are listed as CLINK at https://www.bluetooth.com/specifications/assigned-numbers/company-identifiers
+           the code should be stored as advert[4] => FO and advert[5] => 03
+        */
+        if (advert[4] !== 0xF0 || advert[5] !== 0x03) {
+            // this is not a Wowwee device
+            return false;
+        }
+
+        // check if MiP, all MiPs have the 4th digit set to 3
+        return (advert.length > 3 && advert[3] === 5);
+    }
 
     initialisePermissionsIfRequired() {
         this.hasPermissions()
@@ -56,4 +70,42 @@ export class BluetoothScanner {
             }
         );
     }
+}
+
+const keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+
+function base64_decode(input: string): Array<number> {
+    const orig_input = input;
+    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+    if (orig_input != input)
+        console.error("Warning! Characters outside Base64 range in input string ignored.");
+    if (input.length % 4) {
+        console.error("Error: Input length is not a multiple of 4 bytes.");
+        return [];
+    }
+
+    const output: Array<number> = [];
+
+    let i=0;
+    while (i < input.length) {
+
+        const enc1 = keyStr.indexOf(input.charAt(i++));
+        const enc2 = keyStr.indexOf(input.charAt(i++));
+        const enc3 = keyStr.indexOf(input.charAt(i++));
+        const enc4 = keyStr.indexOf(input.charAt(i++));
+
+        const chr1 = (enc1 << 2) | (enc2 >> 4);
+        const chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+        const chr3 = ((enc3 & 3) << 6) | enc4;
+        
+        output.push(chr1);
+        if (enc3 != 64) {
+            output.push(chr2);
+        }
+        if (enc4 != 64) {
+            output.push(chr3);
+        };
+            
+    }
+    return output;
 }
