656dd82e87f81f391c3720498a866ac4722c3d7f
Sebastian Witalec <sebawita@gmail.com>
2017-01-12 17:35:00 +0000

    Clean up

--- demo/app/pages/accelerometer/accelerometer-page.ts
+++ demo/app/pages/accelerometer/accelerometer-page.ts
@@ -1,8 +1,9 @@
 import { EventData } from "data/observable";
 import { Page } from "ui/page";
+import { View } from "ui/core/view";
+
 import { accelerometerViewModel } from "./accelerometer-view-model";
 
-// Event handler for Page "navigatingTo" event attached in main-page.xml
 export function navigatingTo(args: EventData) {
     // Get the event sender
     var page = <Page>args.object;
