58a8e74c9bf3b5fcdf13a1fd0a59e51f55d35da1
Sebastian Witalec <sebawita@gmail.com>
2017-01-13 16:38:20 +0000

    Moving SideDrawer to a single template

--- demo/app/pages/drawer/drawer-template.ts
+++ demo/app/pages/drawer/drawer-template.ts
@@ -1,63 +1,36 @@
 import {topmost} from "ui/frame";
 
-function sideDrawer(): any {
-    return topmost().currentPage.getViewById("side-drawer");
-}
-function closeDrawer() {
-    var instance = sideDrawer();
-    if (instance) {
-        // console.log("closing drawer");
-        instance.closeDrawer();
-    }
-}
-
 var navigationEntry = {
     moduleName: "pages/controller/controller-page",
-    animated: false,
     clearHistory: true
 };
 
 export function navigateToScanner() {
-    closeDrawer();
-
     navigationEntry.moduleName = "pages/scan/scan-page";
     topmost().navigate(navigationEntry);
 }
 
 export function navigateToArrows() {
-    closeDrawer();
-
     navigationEntry.moduleName = "pages/arrows/arrows-page";
     topmost().navigate(navigationEntry);
 }
 
 export function navigateToAccelerometer() {
-    closeDrawer();
-    
     navigationEntry.moduleName = "pages/accelerometer/accelerometer-page";
     topmost().navigate(navigationEntry);
 }
 
 export function navigateToJoystick() {
-    closeDrawer();
-    
     navigationEntry.moduleName = "pages/joystick/joystick-page";
     topmost().navigate(navigationEntry);
 }
 
 export function navigateToSound() {
-    closeDrawer();
-    
     navigationEntry.moduleName = "pages/sound/sound-page";
     topmost().navigate(navigationEntry);
 }
 
 export function navigateToLights() {
-    closeDrawer();
-    
     navigationEntry.moduleName = "pages/lights/lights-page";
     topmost().navigate(navigationEntry);
 }
-
-
-
